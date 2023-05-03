505a0ff563a7a7b36861a2227a15433aa7f4756d
Maksim Leskin <Quper2405@gmail.com>
2023-05-03 16:43:05 +0300

    style and responsive for hero and about

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import './index.html';
 import 'swiper/scss';
 import 'swiper/scss/pagination';
 import './index.scss';
+import { slidersInit } from './modules/sliders';
 
 
 // use modules
+
+slidersInit('.about__slider', {
+  pagination: {
+    el: '.about__slider-pagination',
+  }
+});
+
 
 const videoBG = document.querySelector('.video-bg');
 
